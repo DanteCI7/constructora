3b6cc64424aa958aa8945ce55fcdfd5b53f85571
DanteCI7 <124225571+DanteCI7@users.noreply.github.com>
2023-06-05 12:25:24 -0600

    Add files via upload

---
+++ admin/js/scroll_review.js
@@ -0,0 +1,13 @@
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 2800,
+  // reset: true,
+});
+
+sr.reveal(
+  ".flow, .redondo, .location-image, .col-md-8, .col-md-4, .col-md-6, .col-2, .col-4, .col-6, .col-8, .col-md-10, .card, .footer-left, .footer-center, .footer-right, .blanco-index, .negro-title, .blanco, .blanco-salud",
+  {
+    origin: "top",
+    interval: 100,
+  }
+);
